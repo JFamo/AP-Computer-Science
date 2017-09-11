1a7d573e52e5cf85547a0f9b2f3c7c87fa988d09
J. Famous <joshua.famous@gmail.com>
2017-09-11 11:07:13 -0400

    BlueJ killed me

---
+++ FirstPrograms/PercentError.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class PercentError {
+    public static void main(String args[]){
+        
+        //vars
+        Scanner scan = new Scanner(System.in);
+        double[] valuesD = new double[0];
+        
+        //input
+        System.out.println("Enter data, seperated by spaces : ");
+        
+        String[] values = scan.nextLine().split(" ");
+        
+        //convert
+        for(String s : values){
+            double[] newArray = new double[valuesD.length + 1];
+            newArray[n] = new double[valuesD.length + 1];
+        }
+        
+    }
+}
