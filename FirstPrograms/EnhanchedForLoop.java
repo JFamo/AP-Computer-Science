14c3eff7c90380f1a682ffb82905168fd3ef1e6b
J. Famous <joshua.famous@gmail.com>
2017-10-06 10:54:38 -0400

    Enhanced for loop program

---
+++ FirstPrograms/EnhanchedForLoop.java
@@ -0,0 +1,13 @@
+public class EnhanchedForLoop {
+    public static void main(String args[]){
+        
+       //vars
+       int[] numbers = {100, -50, 1000, 25000, 1000000, -550, 10000000, -50000, -10, 754125, -1, -250000, -1000000, -50, 0};
+       
+       //loop
+       for(int num : numbers){
+           System.out.print(num + " ");
+       }
+       
+    }
+}
