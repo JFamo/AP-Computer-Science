29df07cb6ad452c32826542a7e7a99967310595f
J. Famous <joshua.famous@gmail.com>
2017-11-07 10:44:52 -0500

    For some reason Jim told us 2 was wrong, even though it's totally right

---
+++ FirstPrograms/GPS.java
@@ -0,0 +1,10 @@
+public class GPS {
+    
+    public GPS (int id){
+        myId = id;
+        num++;
+    }
+    
+    public static int num = 0;
+    public int myId;
+}
---
+++ FirstPrograms/GPSTester.java
@@ -0,0 +1,9 @@
+public class GPSTester {
+    public static void main(String args[]){
+        
+        GPS gp1 = new GPS(1);
+        GPS gp2 = new GPS(3);
+        System.out.print(GPS.num);
+        
+    }
+}
